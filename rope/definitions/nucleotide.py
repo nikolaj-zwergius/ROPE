cd4e59e06ad6999f1d41028acd89e0907b59c284
Nikolaj Holck Zwergius <nikolajzwergius@gmail.com>
2026-07-01 16:05:15 +0200

    allowed for N in RNAbuild

--- rope/definitions/nucleotide.py
+++ rope/definitions/nucleotide.py
@@ -1,6 +1,7 @@
 
 from rope.model.Nucleotide import Nucleotide
 from rope.definitions.rope_def import NT_FOLDER
+from random import choices
 ### Nucleotides
 A=Nucleotide(name = 'A', file = NT_FOLDER/'A.pdb', symbol = 'A')
 C=Nucleotide(name = 'C', file = NT_FOLDER/'C.pdb', symbol = 'C')
@@ -11,7 +12,8 @@ nucleotide_libary:dict[str,Nucleotide] = {
                     A.symbol:A,
                     C.symbol:C,
                     G.symbol:G,
-                    U.symbol:U}
+                    U.symbol:U,
+                    "N":choices([A,C,G,U],k=1)[0]}
 
 
 for nt in nucleotide_libary.values():
