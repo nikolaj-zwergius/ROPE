059974dbe4eb2ea94313d407db5d955806615d44
nikolaj-zwergius <nikolajzwergius@gmail.com>
2026-08-27 15:13:15 +0200

    fixed version problem with tomllib begin 3.11+ now running 3.10+

--- rope/version.py
+++ rope/version.py
@@ -1,6 +1,9 @@
 
 from pathlib import Path
-import tomllib
+try: import tomllib
+except ImportError:
+    import tomli as tomllib
+
 
 def get_version() -> str:
     pyproject = Path(__file__).parents[1] / "pyproject.toml"
